e2123092b8af19d94e9d6c7a6f9c64b82605d777
matthewacha <matthewacha@gmail.com>
2019-06-26 10:25:54 +0300

    ch(setup) setup vue app - create few app - setup vuex for state management - setup vuetify for design

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <div id="logoDiv">
+        <img id="imageIcon" alt="appIcon" src='./assets/appIcon.svg'>
+      </div>
+      <!-- <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> -->
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#nav {
+  padding: 5px;
+  width: 100%;
+  height: 40px;
+  background-color: #2c3e50;
+  #logoDiv {
+    height: 30px;
+    width: 30px;
+    align-self: center;
+    #imageIcon {
+      height: inherit;
+      width: 30px
+    }
+  }
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
